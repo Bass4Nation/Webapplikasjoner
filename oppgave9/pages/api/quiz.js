d3dfad672e5f4cc04b5a78f2f5fad1237009b802
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2021-10-28 18:14:52 +0200

    POST

--- oppgave9/pages/api/quiz.js
+++ oppgave9/pages/api/quiz.js
@@ -41,5 +41,32 @@ const quiz = [
 ]
 
 export default function handler(req, res) {
-    res.status(200).json({ success: true, data: quiz })
+    if (req.method === 'POST') {
+        // tar i mot data som sendes med forespørselen
+        const data = req.body
+
+        // undersøke om request body har key = question
+        if (!data?.question) {
+            // hvis ikke returner 400 Bad Request
+            // sender med feilmelding som vi kan bruke (error: ...)
+            res
+                .status(400)
+                .json({ success: false, error: 'Fyll ut all nødvendig data' })
+        } else {
+            // legger til data i quiz listen vår
+            quiz.push(data)
+
+            // sender status 201 (Created) og den nye oppdaterte listen
+            res.status(201).json({ success: true, data: quiz })
+        }
+    } else if (req.method === 'PUT') {
+        // sender status 405 => metoden er ikke lov
+        res.status(405).end()
+    } else {
+        // håndterer alle andre responser
+        res.status(200).json({ success: true, data: quiz })
+    
+    }
+
 }
+
